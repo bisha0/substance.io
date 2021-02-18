06fac9af07011f8335e3d87913cedb194fc561bc
Ahmed Bashir <bashirebox@gmail.com>
2021-02-18 19:25:04 +0200

    Initial

---
+++ js/buttons.js
@@ -0,0 +1,21 @@
+// js-docs-start button ripple effect
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        let x = e.clientX - e.target.offsetLeft;
+        let y = e.clientY - e.target.offsetTop;
+
+        let ripples = document.createElement("span");
+        ripples.style.left = x + "px";
+        ripples.style.top = y + "px";
+
+        ripples.classList.add("ripple");
+        button.appendChild(ripples);
+
+        setTimeout(() => {
+            ripples.remove();
+        }, 1000);
+    });
+});
+// js-docs-end button ripple effect
