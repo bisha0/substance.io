2f9a8f100e0f1c5f950f9bd1787dcd9021bdae64
Ahmed Bashir <bashirebox@gmail.com>
2021-02-19 08:22:55 +0200

    refactor: index.ts

--- dist/substance.main.js
+++ dist/substance.main.js
@@ -3,23 +3,26 @@ var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.ts ***!
   \**********************/
-// js-docs-start button ripple effect
-var buttons = document.querySelectorAll(".btn");
-buttons.forEach(function (button) {
-    button.addEventListener("click", function (e) {
-        var x = e.clientX - e.target.offsetTop;
-        var y = e.clientY - e.target.offsetTop;
-        var ripples = document.createElement("span");
-        ripples.style.left = x + "px";
-        ripples.style.top = y + "px";
-        ripples.classList.add("ripple");
-        button.appendChild(ripples);
-        setTimeout(function () {
-            ripples.remove();
-        }, 1000);
+function main() {
+    // js-docs-start button ripple effect
+    var buttons = document.querySelectorAll(".btn");
+    buttons.forEach(function (button) {
+        button.addEventListener("click", function (e) {
+            var x = e.clientX - e.target.offsetLeft;
+            var y = e.clientY - e.target.offsetTop;
+            var ripples = document.createElement("span");
+            ripples.style.left = x + "px";
+            ripples.style.top = y + "px";
+            ripples.classList.add("ripple");
+            button.appendChild(ripples);
+            setTimeout(function () {
+                ripples.remove();
+            }, 1000);
+        });
     });
-});
-// js-docs-end button ripple effect
+    // js-docs-end button ripple effect
+}
+main();
 
 /******/ })()
 ;
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,25 @@
-// js-docs-start button ripple effect
-const buttons = document.querySelectorAll(".btn");
+function main() {
+    // js-docs-start button ripple effect
+    const buttons = document.querySelectorAll(".btn");
 
-buttons.forEach((button) => {
-    button.addEventListener("click", (e: any) => {
-        let x = e.clientX - e.target.offsetTop;
-        let y = e.clientY - e.target.offsetTop;
+    buttons.forEach((button) => {
+        button.addEventListener("click", (e: any) => {
+            let x = e.clientX - e.target.offsetLeft;
+            let y = e.clientY - e.target.offsetTop;
 
-        let ripples = document.createElement("span");
-        ripples.style.left = x + "px";
-        ripples.style.top = y + "px";
+            let ripples = document.createElement("span");
+            ripples.style.left = x + "px";
+            ripples.style.top = y + "px";
 
-        ripples.classList.add("ripple");
-        button.appendChild(ripples);
+            ripples.classList.add("ripple");
+            button.appendChild(ripples);
 
-        setTimeout(() => {
-            ripples.remove();
-        }, 1000);
+            setTimeout(() => {
+                ripples.remove();
+            }, 1000);
+        });
     });
-});
-// js-docs-end button ripple effect
+    // js-docs-end button ripple effect
+}
+
+main();
