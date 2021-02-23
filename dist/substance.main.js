a94d228f6ad4f68dfb98f40e1183158f53af41c4
Ahmed Bashir <bashirebox@gmail.com>
2021-02-24 01:56:16 +0200

    refactor: scss
    rename dialog classes to modal
    rename _dialog.scss to _modal.scss
    remove dropdown-toggle class
    add fontawesome cog icon
    remove ul marign & padding styles in _reboot.scss
    remove comments from _variables.scss

--- dist/substance.main.js
+++ dist/substance.main.js
@@ -1,75 +1,43 @@
 "use strict";
 
-// // ts-docs-start loading method
-// function loading(element) {
-//     element.addEventListener("click", () => {
-//         element.classList.add("spinner");
-//         setTimeout(() => {
-//             element.classList.remove("spinner");
-//         }, 2000);
-//     });
-// }
-// // ts-docs-end loading method
-function main() {
-  // ts-docs-start button ripple effect
-  var buttons = document.querySelectorAll(".btn");
-  buttons.forEach(function (button) {
-    button.addEventListener("click", function (e) {
-      var x = e.clientX - e.target.offsetLeft;
-      var y = e.clientY - e.target.offsetTop;
-      var ripples = document.createElement("span");
-      ripples.style.left = x + "px";
-      ripples.style.top = y + "px";
-      ripples.classList.add("ripple");
-      button.appendChild(ripples);
-      setTimeout(function () {
-        ripples.remove();
-      }, 1000);
+var buttons = document.querySelectorAll(".btn");
+buttons.forEach(function (button) {
+  button.addEventListener("click", function (e) {
+    var x = e.clientX - e.target.offsetLeft;
+    var y = e.clientY - e.target.offsetTop;
+    var ripple = document.createElement("span");
+    ripple.style.left = "".concat(x, "px");
+    ripple.style.top = "".concat(y, "px");
+    ripple.classList.add("ripple");
+    button.appendChild(ripple);
+    setTimeout(function () {
+      ripple.remove();
+    }, 1000);
+  });
+});
+var dropdowns = document.querySelectorAll(".dropdown");
+dropdowns.forEach(function (dropdown) {
+  var toggle = dropdown.querySelector('[data-toggle~="dropdown"]');
+  var menu = dropdown.querySelector(".dropdown-menu");
+  toggle.addEventListener("click", function () {
+    menu.classList.toggle("show");
+  });
+  window.addEventListener("click", function (e) {
+    if (!e.target.matches('[data-toggle~="dropdown"]')) {
+      menu.classList.remove("show");
+    }
+  });
+});
+var modalToggles = document.querySelectorAll('[data-toggle~="modal"]');
+modalToggles.forEach(function (toggle) {
+  var modal = document.querySelector(toggle.dataset.target);
+  var closeEls = document.querySelectorAll("[data-close]");
+  toggle.addEventListener("click", function () {
+    modal.classList.toggle("show");
+  });
+  closeEls.forEach(function (closeEl) {
+    closeEl.addEventListener("click", function () {
+      modal.classList.remove("show");
     });
-  }); // ts-docs-end button ripple effect
-  // ts-docs-start navbar-toggle
-
-  var navbars = document.querySelectorAll(".navbar");
-  navbars.forEach(function (navbar) {
-    var toggler = navbar.querySelector(".navbar-toggler");
-    var collapse = navbar.querySelector(".navbar-collapse");
-    toggler.addEventListener("click", function () {
-      collapse.classList.toggle("show");
-    });
-  }); // ts-docs-end navbar-toggle
-  // ts-docs-start dropdown-toggle
-
-  var dropdowns = document.querySelectorAll(".dropdown");
-  dropdowns.forEach(function (dropdown) {
-    var toggle = dropdown.querySelector(".dropdown-toggle");
-    var menu = dropdown.querySelector(".dropdown-menu"); // toggle.classList.add("icon angle-down");
-
-    toggle.classList.add("icon");
-    toggle.classList.add("angle-down");
-    toggle.addEventListener("click", function () {
-      menu.classList.toggle("show");
-    });
-    window.addEventListener("click", function (e) {
-      if (!e.target.matches(".dropdown-toggle")) {
-        menu.classList.remove("show");
-      }
-    });
-  }); // ts-docs-end dropdown-toggle
-  // ts-docs-start dialog-toggle
-
-  var dialogToggles = document.querySelectorAll('[data-toggle~="dialog"]');
-  dialogToggles.forEach(function (toggle) {
-    var dialog = document.querySelector(toggle.dataset.target);
-    var close = dialog.querySelectorAll("[data-close]");
-    toggle.addEventListener("click", function () {
-      dialog.classList.toggle("show");
-    });
-    close.forEach(function (el) {
-      el.addEventListener("click", function () {
-        dialog.classList.remove("show");
-      });
-    });
-  }); // ts-docs-end dialog-toggle
-}
-
-main();
+  });
+});
