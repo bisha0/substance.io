c5348bc338c4ab91f9bd68020a3fd4aea65d90e2
Ahmed Bashir <bashirebox@gmail.com>
2021-02-24 01:58:26 +0200

    refactor js
    restructure syntax

--- js/substance.main.js
+++ js/substance.main.js
@@ -1,90 +1,46 @@
-// // ts-docs-start loading method
-// function loading(element) {
-//     element.addEventListener("click", () => {
-//         element.classList.add("spinner");
-//         setTimeout(() => {
-//             element.classList.remove("spinner");
-//         }, 2000);
-//     });
-// }
-// // ts-docs-end loading method
+const buttons = document.querySelectorAll(".btn");
 
-function main() {
-    // ts-docs-start button ripple effect
-    const buttons = document.querySelectorAll(".btn");
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        let x = e.clientX - e.target.offsetLeft;
+        let y = e.clientY - e.target.offsetTop;
+        let ripple = document.createElement("span");
+        ripple.style.left = `${x}px`;
+        ripple.style.top = `${y}px`;
+        ripple.classList.add("ripple");
+        button.appendChild(ripple);
+        setTimeout(() => {
+            ripple.remove();
+        }, 1000);
+    });
+});
 
-    buttons.forEach((button) => {
-        button.addEventListener("click", (e) => {
-            let x = e.clientX - e.target.offsetLeft;
-            let y = e.clientY - e.target.offsetTop;
+const dropdowns = document.querySelectorAll(".dropdown");
 
-            let ripples = document.createElement("span");
-            ripples.style.left = x + "px";
-            ripples.style.top = y + "px";
+dropdowns.forEach((dropdown) => {
+    const toggle = dropdown.querySelector('[data-toggle~="dropdown"]');
+    const menu = dropdown.querySelector(".dropdown-menu");
+    toggle.addEventListener("click", () => {
+        menu.classList.toggle("show");
+    });
+    window.addEventListener("click", (e) => {
+        if (!e.target.matches('[data-toggle~="dropdown"]')) {
+            menu.classList.remove("show");
+        }
+    });
+});
 
-            ripples.classList.add("ripple");
-            button.appendChild(ripples);
+const modalToggles = document.querySelectorAll('[data-toggle~="modal"]');
 
-            setTimeout(() => {
-                ripples.remove();
-            }, 1000);
+modalToggles.forEach((toggle) => {
+    const modal = document.querySelector(toggle.dataset.target);
+    const closeEls = document.querySelectorAll("[data-close]");
+    toggle.addEventListener("click", () => {
+        modal.classList.toggle("show");
+    });
+    closeEls.forEach((closeEl) => {
+        closeEl.addEventListener("click", () => {
+            modal.classList.remove("show");
         });
     });
-    // ts-docs-end button ripple effect
-
-    // ts-docs-start navbar-toggle
-    const navbars = document.querySelectorAll(".navbar");
-
-    navbars.forEach((navbar) => {
-        const toggler = navbar.querySelector(".navbar-toggler");
-        const collapse = navbar.querySelector(".navbar-collapse");
-
-        toggler.addEventListener("click", () => {
-            collapse.classList.toggle("show");
-        });
-    });
-    // ts-docs-end navbar-toggle
-
-    // ts-docs-start dropdown-toggle
-    const dropdowns = document.querySelectorAll(".dropdown");
-
-    dropdowns.forEach((dropdown) => {
-        const toggle = dropdown.querySelector(".dropdown-toggle");
-        const menu = dropdown.querySelector(".dropdown-menu");
-
-        // toggle.classList.add("icon angle-down");
-        toggle.classList.add("icon");
-        toggle.classList.add("angle-down");
-
-        toggle.addEventListener("click", () => {
-            menu.classList.toggle("show");
-        });
-
-        window.addEventListener("click", (e) => {
-            if (!e.target.matches(".dropdown-toggle")) {
-                menu.classList.remove("show");
-            }
-        });
-    });
-    // ts-docs-end dropdown-toggle
-
-    // ts-docs-start dialog-toggle
-    const dialogToggles = document.querySelectorAll('[data-toggle~="dialog"]');
-
-    dialogToggles.forEach((toggle) => {
-        const dialog = document.querySelector(toggle.dataset.target);
-        const close = dialog.querySelectorAll("[data-close]");
-
-        toggle.addEventListener("click", () => {
-            dialog.classList.toggle("show");
-        });
-        close.forEach((el) => {
-            el.addEventListener("click", () => {
-                dialog.classList.remove("show");
-            });
-        });
-    });
-    // ts-docs-end dialog-toggle
-}
-
-main();
+});
